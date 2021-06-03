f1e3ae6e6bca99bc7245e6396fcebd085cfb644d
Jarfsoft <andresjex@hotmail.com>
2021-06-03 10:44:47 -0600

    Add Buttons style

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import './Button.css';
 
 function Button(props) {
-  const { name, press, key } = props;
+  const { name, press, key, fr } = props;
+  const fraction = fr === '1' ? 'button1' : 'button0';
   return (
-    <button type="button" key={key} onClick={() => press(name)}>
+    <button className={fraction} type="button" key={key} onClick={() => press(name)}>
       {name}
     </button>
   );
@@ -14,6 +16,7 @@ Button.propTypes = {
   name: PropTypes.string.isRequired,
   press: PropTypes.func.isRequired,
   key: PropTypes.string.isRequired,
+  fr: PropTypes.string.isRequired,
 };
 
 export default Button;
--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -2,36 +2,47 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from './Button';
+import './ButtonPanel.css';
 
 export default class ButtonPanel extends Component {
   btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=']
 
-  callButton(x, y, sIH) {
-    return this.btns.slice(x, y).map(
-      (btn) => <Button key={btn} press={sIH} name={btn} />,
+  callButton(x, y, sIH, fraction) {
+    if (fraction === '1') {
+      return this.btns.slice(x, y).map(
+        (btn) => <Button fr={fraction} key={btn} press={sIH} name={btn} />,
+      );
+    }
+
+    return (
+      <>
+        <Button fr="1" key="0" press={sIH} name="0" />
+        <Button fr="1" key="." press={sIH} name="." />
+        <Button fr="0" key="=" press={sIH} name="=" />
+      </>
     );
   }
 
   render() {
     const { onSaveInput } = this.props;
     return (
-      <>
+      <div className="buttonPanel">
         <div>
-          {this.callButton(0, 4, onSaveInput)}
+          {this.callButton(0, 4, onSaveInput, '1')}
         </div>
         <div>
-          {this.callButton(4, 8, onSaveInput)}
+          {this.callButton(4, 8, onSaveInput, '1')}
         </div>
         <div>
-          {this.callButton(8, 12, onSaveInput)}
+          {this.callButton(8, 12, onSaveInput, '1')}
         </div>
         <div>
-          {this.callButton(12, 16, onSaveInput)}
+          {this.callButton(12, 16, onSaveInput, '1')}
         </div>
         <div>
-          {this.callButton(16, 19, onSaveInput)}
+          {this.callButton(16, 19, onSaveInput, '0')}
         </div>
-      </>
+      </div>
     );
   }
 }

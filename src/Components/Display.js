85552dab3f60dbcd070443e28c2179de3f7abd98
Jarfsoft <andresjex@hotmail.com>
2021-06-03 09:46:14 -0600

    Add Display style

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Display.css';
 
 function Display(props) {
   const { result } = props;
@@ -17,7 +18,7 @@ function Display(props) {
   };
 
   return (
-    <div>
+    <div className="display">
       {validateResult()}
     </div>
   );

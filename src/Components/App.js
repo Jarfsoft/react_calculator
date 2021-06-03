c48426928c0ecfc432e0d97d503689c0293dcdb2
Jarfsoft <andresjex@hotmail.com>
2021-06-03 14:52:44 -0600

    Change App method to Class

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
+/* eslint-disable react/destructuring-assignment */
+
+import React from 'react';
 
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../Logic/calculate';
 import './App.css';
 
-function App() {
-  const [data, setData] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+      display: <Display result={this.state.data} />,
+    };
+  }
+  // const [data, setData] = useState({
+  //  total: null,
+  //  next: null,
+  //  operation: null,
+  // });
 
-  const [display, setDisplay] = useState(
-    <Display result={data} />,
-  );
+  // const [display, setDisplay] = useState(
+  //  <Display result={data} />,
+  // );
 
-  const saveInputHandler = (inputData) => {
-    setData(calculate(data, inputData));
-    setDisplay(
-      <Display result={data} />,
-    );
+  saveInputHandler = (inputData) => {
+    const cb = this.state.data;
+    this.setState(calculate(cb, inputData), [React.callback]);
+    this.setState([React.callback], <Display result={this.state.data} />);
   };
 
-  return (
-    <>
-      {display}
-      <ButtonPanel onSaveInput={saveInputHandler} />
-    </>
-  );
+  render() {
+    return (
+      <>
+        {this.state.display}
+        <ButtonPanel onSaveInput={this.saveInputHandler} />
+      </>
+    );
+  }
 }
 
 export default App;
+
+/* eslint-enable react/destructuring-assignment */
